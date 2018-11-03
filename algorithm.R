63b8962ecddd1a921fc61ffc5d7c23235ba78983
Tyelcie <tyelcie@gmail.com>
2018-11-03 21:33:33 +0800

    mehods

--- algorithm.R
+++ algorithm.R
@@ -4,3 +4,11 @@ if(TRUE){
   candidates <- candidates[-match(awards$Phone, candidates$Phone),]
   results <- rbind(results, awards)
 }
+
+# if we do not delet the temperate result
+# for(i in 1:1800){
+#   i
+#   awards <- candidates[sample(candidates$Phone, 1),]
+#   results <- rbind(results, awards)
+# }
+# any(duplicated(results$Phone))
