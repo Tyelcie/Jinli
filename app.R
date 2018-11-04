77977b8434e60fd008cf2f452dd385c5c1e74019
Tyelcie <tyelcie@gmail.com>
2018-11-04 14:21:43 +0800

    test

--- app.R
+++ app.R
@@ -1,35 +1,36 @@
 library(shiny)
 source('helper.R')
-# Define UI for application that draws a histogram
+
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("解螺旋锦鲤"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-        dataTableOutput('candidates'),
-         actionButton('go', 'Go')
+        dataTableOutput('candidates')
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-        DT::dataTableOutput('results')
+        submitButton('go', '抽大奖'),
+        dataTableOutput('results')
       )
    )
 )
-results <- data.frame()
-# Define server logic required to draw a histogram
+
 server <- function(input, output, session) {
-  output$candidates <- DT::renderDataTable(candidates, options = list(pagelength = 10))
+  output$candidates <- renderDataTable(candidates)
   observeEvent(input$go, {
+    
     output$results <- renderDataTable({
       Awards <- candidates[sample(candidates$Phone,1),]
+      candidates <- candidates[-match(awards$Phone, candidates$Phone),]
       results <- rbind(results,Awards)})
   })
   results <- results
 }
 
-# Run the application 
+
 shinyApp(ui = ui, server = server)
--- helper.R
+++ helper.R
@@ -4,7 +4,7 @@ results <- data.frame(Name = NULL, Phone = NULL)
 
 # read
 # read data from inputs
-CastData <- fuction(data){
+CastData <- function(data){
   results <- data.frame(Name = data['Name'],
                         Phone = data['Phone'])
   rownames(results) <- data['id']
@@ -12,7 +12,7 @@ CastData <- fuction(data){
 }
 
 # create default record when click 'new' button
-CreateDefaultRecord <- fuction(){
+CreateDefaultRecord <- function(){
   mydefault <- CastData(list(id = '0', Name = '', Phone = ''))
   return(mydefault)
 }
